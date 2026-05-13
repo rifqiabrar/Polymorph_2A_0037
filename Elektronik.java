8493c1bb37d8a0c5b16c123fbe0133a25111d43d
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:25:29 +0700

    Implementasi Info Garansi

--- Elektronik.java
+++ Elektronik.java
@@ -12,5 +12,11 @@ public class Elektronik extends Produk {
         System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
         return hitungTotalHarga(jmlhBeli) * 0.85;
     }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Garansi: " + masaGaransi + " Tahun");
+    }
     
 }
