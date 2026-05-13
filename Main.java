c5405c251402895fa20e1fa8929942ff2117c993
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 09:54:26 +0700

    Inisialisasi Objek

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         produk p1 = new Elektronik("Samsung", 100000, 5, 2);
-        Produk p2 = new
+        Produk p2 = new Makanan("Bakso", 1000, 100, 3);
+        Kasir kasir = new Kasir("Maman");
     }
     
 }
