c7461b46c2ad8d7e865c12a186aef24aec33fa39
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:24:28 +0700

    Implementasi Hitung Harga Akhir

--- Elektronik.java
+++ Elektronik.java
@@ -6,5 +6,11 @@ public class Elektronik extends Produk {
         super(nama, harga, stok);
         this.masaGaransi = masaGaransi;
     }
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total Harga= " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
     
 }
