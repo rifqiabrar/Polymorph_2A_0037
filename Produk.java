4805b3bc61fb069f9b498a6d2c280e0ed1e885c6
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:28:19 +0700

    Inisialisasi Struktur Dasar dan Atribut

---
+++ Produk.java
@@ -0,0 +1,13 @@
+public abstract class produk {
+
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+    
+}
