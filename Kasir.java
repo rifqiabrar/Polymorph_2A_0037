2e6b359104116954b4aeaf9992f52cc2514c5b05
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:10:17 +0700

    Implementasi Metode Penampilan Identitas

--- Kasir.java
+++ Kasir.java
@@ -4,6 +4,11 @@ public class Kasir {
     public Kasir(String namaKasir) {
         this.namaKasir = namaKasir;
     }
+
+    public void tampilIndentitas() {
+        System.out.println("===== TOKO GARUDA =====");
+        System.out.println("Nama Kasir: " + namaKasir);
+    }
 }
     
 
