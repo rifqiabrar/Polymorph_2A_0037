1059445113badee08bcb8c4d240be6c64f236108
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:23:15 +0700

    Inisialisasi Kelas dan Atribut

---
+++ Elektronik.java
@@ -0,0 +1,10 @@
+public class Elektronik extends Produk {
+    
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+    
+}
