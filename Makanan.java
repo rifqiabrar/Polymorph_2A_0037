efb47c87abbb892c5ed2776e7907d75328ad929d
rifqiabrar <mrifqiabrarr@gmail.com>
2026-05-13 10:16:53 +0700

    Inisialisasi Kelas dan Atribut

---
+++ Makanan.java
@@ -0,0 +1,8 @@
+public class Makanan extends Produk {
+    private int kadaluarsa;
+
+    public Makanan(String nama, double harga, int stok, int kadaluarsa) {
+        super(nama, harga, stok);
+        this.kadaluarsa = kadaluarsa;
+    }
+}
